b62505b655786a6039bb5ea4ad9db9a5e94e521c
Philipp Reh <sefi@s-e-f-i.de>
2013-08-23 17:26:20 +0200

    Migrate iostreams sink and source from sanguis

--- include/alda/net/buffer/circular_receive/source.hpp
+++ include/alda/net/buffer/circular_receive/source.hpp
@@ -21,12 +21,17 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef ALDA_NET_BUFFER_CIRCULAR_RECEIVE_SOURCE_HPP_INCLUDED
 #define ALDA_NET_BUFFER_CIRCULAR_RECEIVE_SOURCE_HPP_INCLUDED
 
+#include <alda/symbol.hpp>
 #include <alda/net/value_type.hpp>
 #include <alda/net/buffer/circular_receive/object_fwd.hpp>
 #include <fcppt/nonassignable.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <boost/iostreams/concepts.hpp>
 #include <ios>
+#include <iosfwd>
 #include <fcppt/config/external_end.hpp>
 
 
@@ -39,14 +44,17 @@ namespace buffer
 namespace circular_receive
 {
 
-class receive_buffer_source
+class source
 {
 	FCPPT_NONASSIGNABLE(
-		receive_buffer_source
+		source
 	);
 public:
 	typedef
 	alda::net::value_type char_type;
+
+	FCPPT_PP_PUSH_WARNING
+	FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
 
 	struct category
 	:
@@ -55,17 +63,22 @@ public:
 	{
 	};
 
+	FCPPT_PP_POP_WARNING
+
+	ALDA_SYMBOL
 	explicit
-	receive_buffer_source(
+	source(
 		alda::net::buffer::circular_receive::object &
 	);
 
+	ALDA_SYMBOL
 	std::streamsize
 	read(
 		char *,
 		std::streamsize
 	);
 
+	ALDA_SYMBOL
 	std::streampos
 	seek(
 		boost::iostreams::stream_offset,
