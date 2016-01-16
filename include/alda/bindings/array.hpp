5f1449c8d49f1a1b49018addd6530c0899eab60b
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-01-16 14:49:46 +0100

    Add generic array binding

--- include/alda/bindings/array.hpp
+++ include/alda/bindings/array.hpp
@@ -13,16 +13,27 @@
 #include <majutsu/raw/element_type.hpp>
 #include <majutsu/raw/integral_size.hpp>
 #include <majutsu/raw/make.hpp>
+#include <majutsu/raw/make_generic.hpp>
 #include <majutsu/raw/needed_size.hpp>
 #include <majutsu/raw/place.hpp>
 #include <majutsu/raw/pointer.hpp>
 #include <majutsu/raw/size_type.hpp>
 #include <majutsu/raw/static_size.hpp>
+#include <majutsu/raw/stream/bind.hpp>
+#include <majutsu/raw/stream/reference.hpp>
+#include <majutsu/raw/stream/result.hpp>
+#include <majutsu/raw/stream/return.hpp>
+#include <fcppt/tag_value.hpp>
+#include <fcppt/algorithm/fold.hpp>
 #include <fcppt/cast/size.hpp>
 #include <fcppt/container/array_size.hpp>
 #include <fcppt/preprocessor/disable_gcc_warning.hpp>
 #include <fcppt/preprocessor/pop_warning.hpp>
 #include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <array>
+#include <boost/mpl/range_c.hpp>
+#include <fcppt/config/external_end.hpp>
 
 
 namespace alda
@@ -121,6 +132,129 @@ make(
 		ret;
 }
 
+template<
+	typename Stream,
+	typename Type,
+	typename Adapted
+>
+majutsu::raw::stream::result<
+	Stream,
+	alda::bindings::array<
+		Type,
+		Adapted
+	>
+>
+make_generic(
+	majutsu::dispatch_type<
+		alda::bindings::array<
+			Type,
+			Adapted
+		>
+	>,
+	majutsu::dispatch_type<
+		Stream
+	>,
+	majutsu::raw::stream::reference<
+		Stream
+	> _stream
+)
+{
+	typedef
+	majutsu::raw::stream::result<
+		Stream,
+		alda::bindings::array<
+			Type,
+			Adapted
+		>
+	>
+	result_type;
+
+	// TODO: Improve this by breaking out early
+	// TODO: We should fold the array
+	return
+		fcppt::algorithm::fold(
+			boost::mpl::range_c<
+				std::size_t,
+				0,
+				fcppt::container::array_size<
+					Type
+				>::value
+			>{},
+			majutsu::raw::stream::return_<
+				Stream
+			>(
+				majutsu::raw::element_type<
+					alda::bindings::array<
+						Type,
+						Adapted
+					>
+				>()
+			),
+			[
+				&_stream
+			](
+				auto const _index,
+				result_type &&_result
+			)
+			{
+				return
+					majutsu::raw::stream::bind<
+						Stream
+					>(
+						std::move(
+							_result
+						),
+						[
+							&_stream,
+							_index
+						](
+							Type &&_array
+						)
+						{
+							return
+								majutsu::raw::stream::bind<
+									Stream
+								>(
+									majutsu::raw::make_generic<
+										Stream,
+										Adapted
+									>(
+										_stream
+									),
+									[
+										&_array,
+										_index
+									](
+										majutsu::raw::element_type<
+											Adapted
+										> &&_element
+									)
+									{
+										std::get<
+											fcppt::tag_value(
+												_index
+											)
+										>(
+											_array
+										) =
+											_element;
+
+										return
+											majutsu::raw::stream::return_<
+												Stream
+											>(
+												std::move(
+													_array
+												)
+											);
+									}
+								);
+						}
+					);
+				}
+			);
+}
+
 }
 }
 
@@ -143,6 +277,7 @@ struct static_size<
 	>
 >
 :
+// FIXME: We have to check if Type has a static size
 majutsu::raw::integral_size<
 	// Can't use mpl::multiplies here because std::integral_constant doesn't work
 	fcppt::cast::size<
