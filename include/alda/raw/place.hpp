3a9b67bf133a7b58cc6b894e5452a9e69c2b9082
Carl Philipp Reh <carlphilippreh@gmail.com>
2023-07-30 17:01:52 +0200

    Remove useless push/pop warning

--- include/alda/raw/place.hpp
+++ include/alda/raw/place.hpp
@@ -9,23 +9,15 @@
 #include <alda/raw/dispatch_value.hpp>
 #include <alda/raw/element_type.hpp>
 #include <alda/raw/pointer.hpp>
-#include <fcppt/preprocessor/ignore_unsafe_buffer_usage.hpp>
-#include <fcppt/preprocessor/pop_warning.hpp>
-#include <fcppt/preprocessor/push_warning.hpp>
 
 namespace alda::raw
 {
-
-FCPPT_PP_PUSH_WARNING
-FCPPT_PP_IGNORE_UNSAFE_BUFFER_USAGE
 
 template <typename Type>
 inline void place(alda::raw::element_type<Type> const &_value, alda::raw::pointer const _data)
 {
   return place(alda::raw::dispatch_value<Type>(), _value, _data);
 }
-
-FCPPT_PP_POP_WARNING
 
 }
 
