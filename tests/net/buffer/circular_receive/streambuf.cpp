6e246809295c1294de4a4a506e4f741c10be0775
Carl Philipp Reh <carlphilippreh@gmail.com>
2023-10-02 14:16:45 +0200

    Fix more clang-tidy 17 warnings

--- tests/net/buffer/circular_receive/streambuf.cpp
+++ tests/net/buffer/circular_receive/streambuf.cpp
@@ -3,20 +3,19 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <alda/net/value_type.hpp>
 #include <alda/net/buffer/max_receive_size.hpp>
 #include <alda/net/buffer/circular_receive/part.hpp>
 #include <alda/net/buffer/circular_receive/streambuf.hpp>
 #include <fcppt/catch/begin.hpp>
 #include <fcppt/catch/end.hpp>
-#include <fcppt/container/raw_vector/comparison.hpp>
-#include <fcppt/container/raw_vector/output.hpp>
+#include <fcppt/container/raw_vector/comparison.hpp> // NOLINT(misc-include-cleaner)
+#include <fcppt/container/raw_vector/output.hpp> // NOLINT(misc-include-cleaner)
 #include <fcppt/io/buffer.hpp>
 #include <fcppt/io/get.hpp>
 #include <fcppt/io/optional_buffer.hpp>
 #include <fcppt/io/read_chars.hpp>
 #include <fcppt/optional/object.hpp>
-#include <fcppt/optional/output.hpp>
+#include <fcppt/optional/output.hpp> // NOLINT(misc-include-cleaner)
 #include <fcppt/config/external_begin.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <iterator>
