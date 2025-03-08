51c9b7b3a38633615259237fddf90a9ffa5c776d
Carl Philipp Reh <carlphilippreh@gmail.com>
2025-03-08 13:04:30 +0100

    Ignore some clang-20 warnings

--- include/alda/raw/detail/copy_n.hpp
+++ include/alda/raw/detail/copy_n.hpp
@@ -9,6 +9,9 @@
 #include <alda/raw/const_pointer.hpp>
 #include <alda/raw/pointer.hpp>
 #include <alda/raw/size_type.hpp>
+#include <fcppt/preprocessor/ignore_unsafe_buffer_usage_in_libc_call.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <cstring>
 #include <fcppt/config/external_end.hpp>
@@ -21,7 +24,10 @@ inline void copy_n(
     alda::raw::size_type const _sz,
     alda::raw::pointer const _dest)
 {
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_IGNORE_UNSAFE_BUFFER_USAGE_IN_LIBC_CALL
   std::memcpy(_dest, _src, _sz);
+FCPPT_PP_POP_WARNING
 }
 
 }
